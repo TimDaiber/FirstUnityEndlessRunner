8048fdad98fbee42ce672404b0e09a6670bf0b41
Timber90 <tdaiber90@hotmail.com>
2016-04-03 12:36:42 +0100

    Added comments to Texture Animation script and tiledBackground script

--- MyGame/Assets/Scripts/TextureAnimation.cs
+++ MyGame/Assets/Scripts/TextureAnimation.cs
@@ -3,23 +3,27 @@ using System.Collections;
 
 public class TextureAnimation : MonoBehaviour {
 
-
+    //creates properties that represent the speed the animation will be animated in
     public Vector2 speed = Vector2.zero;
+    // keeps track of the the default offset position of the texture
     private Vector2 offset = Vector2.zero;
+    // stores reference to the material
     private Material material;
 
 	// Use this for initialization
 	void Start () {
+        //gets reference of the material of the quad
         material = GetComponent<Renderer>().material;
-
+        // sets up offset
+        // points to the materials own offset
         offset = material.GetTextureOffset("_MainTex");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        //increments the offset
         offset += speed * Time.deltaTime;
-
+        // applies the incremented offset to the material
         material.SetTextureOffset("_MainTex", offset);
 
 	}
--- MyGame/Assets/Scripts/TiledBackground.cs
+++ MyGame/Assets/Scripts/TiledBackground.cs
@@ -2,20 +2,22 @@
 using System.Collections;
 
 public class TiledBackground : MonoBehaviour {
+    // creating public variables
     public int size = 32;
-
-
     public bool scaleHorizontally = true;
     public bool scaleVertically = true;
+
 	void Start () {
-
-
-
+        // calulating new width and height
+        // rounds up to the highest value number using Math class and ceil
+        // 
         var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil(Screen.width / (size * PixelPervectCamera.scale));
         var newHeight = !scaleVertically ? 1 : Mathf.Ceil(Screen.height / (size * PixelPervectCamera.scale));
-
+        //changes the scale of the quad based on new width and new height
         transform.localScale = new Vector3(newWidth * size, newHeight * size, 1);
-
+        // Tells new material that it has a new scale
+        // gets component renderer 
+        // get reference of the material and gets the new scale
         GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth , newHeight , 1);
 
 	}
