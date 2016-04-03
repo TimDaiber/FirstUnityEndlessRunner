bfc815f7aa6389b31dc94e5e58c9b220a0b8bb7b
Timber90 <tdaiber90@hotmail.com>
2016-04-03 16:22:49 +0100

    Comments added to jump and input state scripts

--- MyGame/Assets/Scripts/InputState.cs
+++ MyGame/Assets/Scripts/InputState.cs
@@ -2,17 +2,20 @@
 using System.Collections;
 
 public class InputState : MonoBehaviour {
-
+    // creating variables
     public bool actionButton;
     public float absVelX = 0f;
     public float absVelY = 0f;
+    // bool to determain if player is standing
     public bool standing;
+    // Keeps track of velosity (if standing or not)
     public float standingThreshold = 1;
-
+    // Creates a Reference
     private Rigidbody2D body2d;
 
     void Awake()
     {
+        // gets component of Ridgid coponent
         body2d = GetComponent<Rigidbody2D>();
 
     }
@@ -20,14 +23,18 @@ public class InputState : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // allows player jump on any key
         actionButton = Input.anyKeyDown;
 	}
 
+    // calculates if player is standing or not
     void FixedUpdate()
     {
+        // convert values to absolute values
         absVelX = System.Math.Abs(body2d.velocity.x);
         absVelY = System.Math.Abs(body2d.velocity.y);
-
+        // tests if player is standing
+        //if not moving on y axis = standing
         standing = absVelY <= standingThreshold;
     }
 }
--- MyGame/Assets/Scripts/Jump.cs
+++ MyGame/Assets/Scripts/Jump.cs
@@ -2,29 +2,30 @@
 using System.Collections;
 
 public class Jump : MonoBehaviour {
-
+    // setting variable to determain jump speed and forward jump distance
     public float jspeed = 240f;
     public float fspeed = 20;
-
-
-
-
+    // creating references 
     private Rigidbody2D body2d;
     private InputState inputState;
 
 
         void Awake()
         {
+            //setting variable to there components
             body2d = GetComponent<Rigidbody2D>();
             inputState = GetComponent<InputState>();
         }
 	
 	// Update is called once per frame
 	void Update () {
+        // if player is standing
         if (inputState.standing)
         {
+            // if action button pressed
             if (inputState.actionButton)
             {
+                // making the player jump up and forward
                 body2d.velocity = new Vector2(transform.position.x < 0 ? fspeed : 0, jspeed);
             }
         }
